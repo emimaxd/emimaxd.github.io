c671d6db1f27d8aa5879e5e9cea2e8b4be553878
emimaxd <156070329+emimaxd@users.noreply.github.com>
2024-01-09 20:26:51 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function foncRouge() {
+    let balp= document.querySelector("#monPara")
+    balp.classList.remove("vert");
+    balp.classList.add("rouge");
+}
+function foncVert() {
+    let balp= document.querySelector("#monPara")
+    balp.classList.remove("rouge");
+    balp.classList.add("vert");
+}
